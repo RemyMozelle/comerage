41487149cbee5083826d857325ad48a45927b537
timoz <timoz974@hotmail.fr>
2018-10-04 20:01:47 +0400

    add realtime of nbmax char disabled send button on createArticle

--- public/js/sidenav.js
+++ public/js/sidenav.js
@@ -8,11 +8,24 @@ document.addEventListener("DOMContentLoaded", function() {
 let carractereMax = 200;
 const inputbody = document.querySelector("#body");
 const nb_car_max = document.querySelector("#nb_car_max");
+const send_restrict = document.querySelector(".send_restrict");
+
 let compteur = 200;
 inputbody.addEventListener("keyup", e => {
   compteur = carractereMax - inputbody.value.length;
   if (e.key === "Backspace") {
     compteur + 1;
   }
-  nb_car_max.innerHTML = "vous avez encore " + compteur + " maximum";
+  if (e.key === "Delete") {
+    compteur + 1;
+  }
+  if (compteur > 200 || compteur < 1) {
+    nb_car_max.innerHTML = "vous ne pouvez pas dépasser 200 carractères";
+    nb_car_max.style.color = "red";
+    send_restrict.setAttribute("disabled", "");
+  } else {
+    nb_car_max.innerHTML = "il vous reste encore " + compteur + " carractères";
+    nb_car_max.style.color = "black";
+    send_restrict.removeAttribute("disabled");
+  }
 });
